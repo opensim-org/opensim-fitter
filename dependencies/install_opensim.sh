dce87d48dfebc867015a22083ccbc3ab783af217
nickbianco <nbianco@stanford.edu>
2026-06-04 17:28:59 -0500

    Switch OpenSim build generator to Ninja to allow relative directory in builds

--- dependencies/install_opensim.sh
+++ dependencies/install_opensim.sh
@@ -9,32 +9,30 @@ NUM_JOBS=24
 MOCO="off"
 ORG="nickbianco"
 BRANCH="opensim_fitter"
-GENERATOR="Unix Makefiles"
+GENERATOR="Ninja"
 PYTHON_ROOT_DIR=$1
-
-# Set the working directory.
 WORKING_DIR="$(pwd)/opensim"
 if [ -d "$WORKING_DIR" ]; then
-    sudo rm -r $WORKING_DIR
+    sudo rm -r "$WORKING_DIR"
 fi
-mkdir -p $WORKING_DIR
+mkdir -p "$WORKING_DIR"
 
 # Get opensim-core.
-git clone https://github.com/$ORG/opensim-core.git $WORKING_DIR/opensim-core
-cd $WORKING_DIR/opensim-core
+git clone https://github.com/$ORG/opensim-core.git "$WORKING_DIR/opensim-core"
+cd "$WORKING_DIR/opensim-core"
 git checkout $BRANCH
 
 # Build opensim-core dependencies.
-mkdir -p $WORKING_DIR/opensim-core/dependencies/build
-cd $WORKING_DIR/opensim-core/dependencies/build
-cmake $WORKING_DIR/opensim-core/dependencies -DCMAKE_INSTALL_PREFIX=$WORKING_DIR/opensim_dependencies_install/ -DSUPERBUILD_ezc3d=off -DOPENSIM_WITH_CASADI=$MOCO -DOPENSIM_WITH_TROPTER=$MOCO -DBUILD_PYTHON_WRAPPING=on -DPython3_ROOT_DIR=$PYTHON_ROOT_DIR
+mkdir -p "$WORKING_DIR/opensim-core/dependencies/build"
+cd "$WORKING_DIR/opensim-core/dependencies/build"
+cmake "$WORKING_DIR/opensim-core/dependencies" -DCMAKE_INSTALL_PREFIX="$WORKING_DIR/opensim_dependencies_install/" -DSUPERBUILD_ezc3d=off -DOPENSIM_WITH_CASADI=$MOCO -DOPENSIM_WITH_TROPTER=$MOCO -DBUILD_PYTHON_WRAPPING=on -DPython3_ROOT_DIR="$PYTHON_ROOT_DIR"
 cmake . -LAH
 cmake --build . --config $DEBUG_TYPE -j$NUM_JOBS
 
 
 # Build and install opensim-core.
-mkdir -p $WORKING_DIR/opensim-core/build
-cd $WORKING_DIR/opensim-core/build
-cmake $WORKING_DIR/opensim-core -G"$GENERATOR" -DOPENSIM_DEPENDENCIES_DIR=$WORKING_DIR/opensim_dependencies_install/ -DOPENSIM_C3D_PARSER=None -DBUILD_TESTING=off -DCMAKE_INSTALL_PREFIX=$WORKING_DIR/opensim_core_install -DOPENSIM_INSTALL_UNIX_FHS=off -DOPENSIM_WITH_CASADI=$MOCO -DOPENSIM_WITH_TROPTER=$MOCO -DBUILD_PYTHON_WRAPPING=on -DPython3_ROOT_DIR=$PYTHON_ROOT_DIR
+mkdir -p "$WORKING_DIR/opensim-core/build"
+cd "$WORKING_DIR/opensim-core/build"
+cmake "$WORKING_DIR/opensim-core" -G"$GENERATOR" -DOPENSIM_DEPENDENCIES_DIR="$WORKING_DIR/opensim_dependencies_install/" -DOPENSIM_C3D_PARSER=None -DBUILD_TESTING=off -DCMAKE_INSTALL_PREFIX="$WORKING_DIR/opensim_core_install" -DOPENSIM_INSTALL_UNIX_FHS=off -DOPENSIM_WITH_CASADI=$MOCO -DOPENSIM_WITH_TROPTER=$MOCO -DBUILD_PYTHON_WRAPPING=on -DPython3_ROOT_DIR="$PYTHON_ROOT_DIR"
 cmake --build . --config $DEBUG_TYPE -j$NUM_JOBS
 cmake --install .
