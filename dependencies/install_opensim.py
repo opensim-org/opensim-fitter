a736b07cb271482c2995e6a0a2d3567bd7dc5931
nickbianco <nbianco@stanford.edu>
2026-06-04 17:31:02 -0500

    Fix merge conflict

--- dependencies/install_opensim.py
+++ dependencies/install_opensim.py
@@ -7,20 +7,10 @@ with open('config.yaml') as f:
 
 # Build OpenSim
 python_root_dir = config['python_root_dir']
-<<<<<<< Updated upstream
 cwd = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 subprocess.run(['bash', 'install_opensim.sh', python_root_dir], check=True, cwd=cwd)
 
 # Install the OpenSim Python package in the current environment.
 package = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'opensim',
                        'opensim_core_install', 'sdk', 'Python', '.')
-=======
-cwd = os.path.dirname(os.path.abspath(__file__))
-subprocess.run(['bash', 'install_opensim.sh', python_root_dir], check=True, cwd=cwd)
-
-# Install the OpenSim Python package in the current environment.
-opensim_build_dir = os.path.join(cwd, 'opensim_build')
-package = os.path.join(opensim_build_dir, 'opensim_core_install', 'sdk', 'Python', '.')
->>>>>>> Stashed changes
 subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
